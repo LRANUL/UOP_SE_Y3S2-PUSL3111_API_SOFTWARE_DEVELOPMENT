d23544290913b97bf9a76ea07e655cceaa600447
Ranul Ladduwahetty <10673986@students.plymouth.ac.uk>
2021-03-01 12:13:57 +0530

    10673986 - NodeJS Server v2
    - Added user fetch, update and delete by NIC

--- programs/workspace/cov-track_server-side-application/src/routes/covTrackRoutes.js
+++ programs/workspace/cov-track_server-side-application/src/routes/covTrackRoutes.js
@@ -1,7 +1,22 @@
-import { addNewUser } from "../controllers/covTrackController";
+import {
+  addNewUser,
+  getUsers,
+  getUsersFromNIC,
+  updateUserFromNIC,
+  removeUserFromNIC
+} from "../controllers/covTrackController";
 
 const routes = (app) => {
-  app.route("/register").post(addNewUser);
+  // For deleting user from NIC
+  app.route("/user-delete/:nic").delete(removeUserFromNIC);
+  // For updating user from NIC
+  app.route("/user-update/:nic").put(updateUserFromNIC);
+  // For getting user from NIC
+  app.route("/user/:nic").get(getUsersFromNIC);
+  // For getting all users
+  app.route("/get-users").get(getUsers);
+  // For register a user with CovTrack
+  app.route("/user-register").post(addNewUser);
 };
 
 export default routes;
