25e205872229d21a15c52be57c88a5d94c3f0233
LRANUL <10673986@students.plymouth.ac.uk>
2021-03-08 21:22:48 +0530

    10673986 - Login Screen
    - Added font assets and logo designed by @RyanCargan
    - Added background image designed by freepik to be added to profile credits
    - Added extra dependencies for support

--- programs/workspace/cov-track_client-side-application/react-native-expo-client/cov-track/App.js
+++ programs/workspace/cov-track_client-side-application/react-native-expo-client/cov-track/App.js
@@ -1,21 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import Login from "./src/screens/Login";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const DrawerNavigation = createDrawerNavigator({
+  Login: Login,
+
+});
+
+const StackNavigation = createStackNavigator(
+  {
+    DrawerNavigation: {
+      screen: DrawerNavigation
+    },
+    Login: Login,
+
+  },
+  {
+    headerMode: "none"
+  }
+);
+const AppContainer = createAppContainer(StackNavigation);
+
+function App() {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+  if (!isLoadingComplete) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
+  } else {
+    return isLoadingComplete ? <AppContainer /> : <AppLoading />;
+  }
+}
+async function loadResourcesAsync() {
+  await Promise.all([
+    Font.loadAsync({
+      "roboto-300": require("./src/assets/fonts/roboto-300.ttf"),
+      "roboto-700": require("./src/assets/fonts/roboto-700.ttf"),
+      "roboto-regular": require("./src/assets/fonts/roboto-regular.ttf")
+    })
+  ]);
+}
+function handleLoadingError(error) {
+  console.warn(error);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function handleFinishLoading(setLoadingComplete) {
+  setLoadingComplete(true);
+}
+
+export default App;
