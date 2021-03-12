f8ac8d8447fd55c529d768f840d85e262c52f1a7
IshanC7 <49109028+TheNooobC7@users.noreply.github.com>
2021-03-12 10:09:57 +0530

    10673936 - reducing folders

---
+++ programs/workspace/cov-track_management-system-client-side-application/cov-track-manager/src/app/health-inspection/health-inspection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-health-inspection',
+  templateUrl: './health-inspection.component.html',
+  styleUrls: ['./health-inspection.component.css']
+})
+export class HealthInspectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
