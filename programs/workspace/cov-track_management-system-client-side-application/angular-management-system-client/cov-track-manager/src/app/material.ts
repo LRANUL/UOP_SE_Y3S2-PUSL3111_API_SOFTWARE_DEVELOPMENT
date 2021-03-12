8432a13dd5fc3481e579e4c57c2999f12d201333
nnwijesuriya <51411336+nnwijesuriya@users.noreply.github.com>
2021-03-12 09:10:45 +0530

    10674049 - Angular Material

---
+++ programs/workspace/cov-track_management-system-client-side-application/angular-management-system-client/cov-track-manager/src/app/material.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
+
+@NgModule({
+  imports: [MatSliderModule],
+  exports: [MatSliderModule],
+})
+
+export class MaterialModule {}
