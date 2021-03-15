c5d7c6c1bb4e3fbbf5bcf0a8322c5756338185a8
Ranul Ladduwahetty <10673986@students.plymouth.ac.uk>
2021-03-15 18:20:37 +0530

    10673986 - Dashboard Screen v2
    - Adjustments to fix small screen devices

--- programs/workspace/cov-track_client-side-application/react-native-expo-client/cov-track/src/screens/Dashboard.js
+++ programs/workspace/cov-track_client-side-application/react-native-expo-client/cov-track/src/screens/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {
   StyleSheet,
   View,
+  StatusBar,
   Image,
   ImageBackground,
   TouchableOpacity,
@@ -12,6 +13,7 @@ import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommun
 function Dashboard(props) {
   return (
     <View style={styles.container}>
+      <StatusBar animated />
       <ImageBackground
         source={require("../assets/images/4324939.jpg")}
         resizeMode="cover"
@@ -19,35 +21,43 @@ function Dashboard(props) {
         imageStyle={styles.image_imageStyle}
       >
         <TouchableOpacity style={styles.profilebtn}>
-          <MaterialCommunityIconsIcon
-            name="account-circle"
-            style={styles.profileicn}
-          ></MaterialCommunityIconsIcon>
-          <Text style={styles.profile}>Profile</Text>
+          <View style={styles.profileicnStack}>
+            <MaterialCommunityIconsIcon
+              name="account-circle"
+              style={styles.profileicn}
+            ></MaterialCommunityIconsIcon>
+            <Text style={styles.profile}>Profile</Text>
+          </View>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.checkinbtn}>
-          <MaterialCommunityIconsIcon
-            name="qrcode-scan"
-            style={styles.checkinicn}
-          ></MaterialCommunityIconsIcon>
-          <Text style={styles.checkIn}>Check in</Text>
+        <TouchableOpacity style={styles.checkinbtn}
+        onPress={() => props.navigation.navigate('CheckIn')}>
+          <View style={styles.checkinicnStack}>
+            <MaterialCommunityIconsIcon
+              name="qrcode-scan"
+              style={styles.checkinicn}
+            ></MaterialCommunityIconsIcon>
+            <Text style={styles.checkIn}>Check in</Text>
+          </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.checkoutbtn}>
-          <MaterialCommunityIconsIcon
-            name="qrcode"
-            style={styles.checkouticn}
-          ></MaterialCommunityIconsIcon>
-          <Text style={styles.checkOut}>Check out</Text>
+          <View style={styles.checkouticnStack}>
+            <MaterialCommunityIconsIcon
+              name="qrcode"
+              style={styles.checkouticn}
+            ></MaterialCommunityIconsIcon>
+            <Text style={styles.checkOut}>Check out</Text>
+          </View>
         </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.historybtn}
+        <TouchableOpacity style={styles.historybtn}
           onPress={() => props.navigation.navigate('History')}
         >
-          <MaterialCommunityIconsIcon
-            name="history"
-            style={styles.historyicn}
-          ></MaterialCommunityIconsIcon>
-          <Text style={styles.history}>History</Text>
+          <View style={styles.historyicnStack}>
+            <MaterialCommunityIconsIcon
+              name="history"
+              style={styles.historyicn}
+            ></MaterialCommunityIconsIcon>
+            <Text style={styles.history}>History</Text>
+          </View>
         </TouchableOpacity>
       </ImageBackground>
     </View>
@@ -63,95 +73,131 @@ const styles = StyleSheet.create({
   },
   image_imageStyle: {},
   profilebtn: {
-    width: 302,
-    height: 148,
+    width: 290,
+    height: 116,
     backgroundColor: "rgba(74,144,226,0.9)",
     borderRadius: 17,
-    marginTop: 87,
+    marginTop: 68,
     alignSelf: "center"
   },
   profileicn: {
+    top: 0,
+    position: "absolute",
     color: "rgba(255,255,255,1)",
     fontSize: 60,
+    left: 19,
     height: 66,
-    width: 60,
-    marginTop: 10,
-    alignSelf: "center"
+    width: 60
   },
   profile: {
+    top: 59,
+    position: "absolute",
     fontFamily: "roboto-300",
     color: "#121212",
-    fontSize: 35,
+    fontSize: 30,
     textAlign: "center",
-    alignSelf: "center"
+    left: 0
+  },
+  profileicnStack: {
+    width: 97,
+    height: 101,
+    marginTop: 2,
+    marginLeft: 96
   },
   checkinbtn: {
-    width: 302,
-    height: 148,
+    width: 290,
+    height: 117,
     backgroundColor: "rgba(126,211,33,0.9)",
     borderRadius: 17,
-    marginTop: 23,
+    marginTop: 19,
     alignSelf: "center"
   },
   checkinicn: {
+    top: 0,
+    position: "absolute",
     color: "rgba(255,255,255,1)",
     fontSize: 60,
+    left: 35,
     height: 66,
-    width: 60,
-    marginTop: 25,
-    alignSelf: "center"
+    width: 60
   },
   checkIn: {
+    top: 61,
+    position: "absolute",
     fontFamily: "roboto-300",
     color: "#121212",
-    fontSize: 35,
+    fontSize: 30,
     textAlign: "center",
-    alignSelf: "center"
+    left: 0
+  },
+  checkinicnStack: {
+    width: 131,
+    height: 103,
+    marginTop: 5,
+    marginLeft: 80
   },
   checkoutbtn: {
-    width: 302,
-    height: 148,
+    width: 290,
+    height: 117,
     backgroundColor: "rgba(248,231,28,0.9)",
     borderRadius: 17,
-    marginTop: 25,
+    marginTop: 20,
     alignSelf: "center"
   },
   checkouticn: {
+    top: 0,
+    position: "absolute",
     color: "rgba(255,255,255,1)",
     fontSize: 60,
+    left: 47,
     height: 66,
-    width: 60,
-    marginTop: 24,
-    alignSelf: "center"
+    width: 60
   },
   checkOut: {
+    top: 58,
+    position: "absolute",
     fontFamily: "roboto-300",
     color: "#121212",
-    fontSize: 35,
+    fontSize: 30,
     textAlign: "right",
-    alignSelf: "center"
+    left: 0
+  },
+  checkouticnStack: {
+    width: 154,
+    height: 100,
+    marginTop: 3,
+    marginLeft: 68
   },
   historybtn: {
-    width: 302,
-    height: 148,
+    width: 290,
+    height: 117,
     backgroundColor: "rgba(155,155,155,0.9)",
     borderRadius: 17,
-    marginTop: 29,
+    marginTop: 22,
     alignSelf: "center"
   },
   historyicn: {
+    top: 0,
+    position: "absolute",
     color: "rgba(255,255,255,1)",
     fontSize: 60,
+    left: 24,
     height: 66,
-    width: 60,
-    marginTop: 30,
-    alignSelf: "center"
+    width: 60
   },
   history: {
+    top: 56,
+    position: "absolute",
     fontFamily: "roboto-300",
     color: "#121212",
-    fontSize: 35,
-    alignSelf: "center"
+    fontSize: 30,
+    left: 0
+  },
+  historyicnStack: {
+    width: 110,
+    height: 98,
+    marginTop: 6,
+    marginLeft: 91
   }
 });
 
