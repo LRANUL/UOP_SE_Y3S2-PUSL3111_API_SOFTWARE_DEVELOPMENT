2a4fd2d8e1f71767e092709a01af216e8db53a40
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-04-22 13:11:19 +0530

    10673969 - Client-side - Mobile App - Dashboard - v2
    - Improved user interface of the 'Dashboard' screen.

--- programs/workspace/cov-track_client-side-application/cov-track/src/screens/Dashboard.js
+++ programs/workspace/cov-track_client-side-application/cov-track/src/screens/Dashboard.js
@@ -10,6 +10,7 @@ import {
   Platform
 } from "react-native";
 import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommunityIcons";
+const userIcon = require('../assets/images/user-icon.png'); 
 
 function Dashboard(props) {
   let mobileView = true
@@ -26,195 +27,271 @@ function Dashboard(props) {
   return (
     <View style={styles.container}>
       <StatusBar animated />
+      <View style={styles.firstBlock}>
+        <View style={styles.menuIconBlock}>
+          <TouchableOpacity>
+            <MaterialCommunityIconsIcon 
+              name="menu" style={styles.menuIcon}>
+            </MaterialCommunityIconsIcon>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.userBanner}>
+          <View style={styles.profileIconBlock}>
+            <Image source={userIcon} style={styles.userIcon} />
+          </View>
+          <View style={styles.greetingTextBlock}>
+            <Text style={styles.greetingText}>
+              Welcome Back, {"\n"}Lucas
+            </Text>
+          </View>
+        </View>
+      </View>
       <ImageBackground
         source={require("../assets/images/4324939.jpg")}
         resizeMode="cover"
-        style={styles.image}
-        imageStyle={styles.image_imageStyle}
+        style={styles.secondBlock}
+        imageStyle={styles.secondBlock}
       >
-        <TouchableOpacity style={styles.profilebtn} >
-          <View style={styles.profileicnStack}>
-            <MaterialCommunityIconsIcon
-              name="account-circle"
-              style={styles.profileicn}
-            ></MaterialCommunityIconsIcon>
-            <Text style={styles.profile}>Profile</Text>
-          </View>
-        </TouchableOpacity>
-        {mobileView ?
-          <TouchableOpacity style={styles.checkinbtn}
-            onPress={() => props.navigation.navigate('CheckIn')}>
-            <View style={styles.checkinicnStack}>
-              <MaterialCommunityIconsIcon
-                name="qrcode-scan"
-                style={styles.checkinicn}
-              ></MaterialCommunityIconsIcon>
-              <Text style={styles.checkIn}>Check in</Text>
+        <View style={styles.tileBlock}>
+          {mobileView ? 
+            <TouchableOpacity
+              onPress={() => props.navigation.navigate('CheckIn')}>
+              <View style={styles.checkInTile}>
+                <View style={styles.tileIcon}>
+                  <MaterialCommunityIconsIcon 
+                    name="qrcode-scan" style={styles.tileIcon}>
+                  </MaterialCommunityIconsIcon>
+                </View>
+                <View>
+                  <Text style={styles.tileText}>CHECK-IN</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            : null}
+          {mobileView ? 
+            <TouchableOpacity 
+              onPress={() => {}}>
+              <View style={styles.checkOutTile}>
+                <View style={styles.tileIcon}>
+                  <MaterialCommunityIconsIcon 
+                    name="qrcode" style={styles.tileIcon}>
+                  </MaterialCommunityIconsIcon>
+                </View>
+                <View>
+                  <Text style={styles.tileText}>CHECK-OUT</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          : null}
+          <TouchableOpacity 
+            onPress={() => props.navigation.navigate('History')}>
+            <View style={styles.historyTile}>
+              <View style={styles.tileIcon}>
+                <MaterialCommunityIconsIcon 
+                  name="history" style={styles.tileIcon}>
+                </MaterialCommunityIconsIcon>
+              </View>
+              <View>
+                <Text style={styles.tileText}>HISTORY</Text>
+              </View>
             </View>
           </TouchableOpacity>
-          : null}
-        {mobileView ?
-          <TouchableOpacity style={styles.checkoutbtn}>
-            <View style={styles.checkouticnStack}>
-              <MaterialCommunityIconsIcon
-                name="qrcode"
-                style={styles.checkouticn}
-              ></MaterialCommunityIconsIcon>
-              <Text style={styles.checkOut}>Check out</Text>
+          <TouchableOpacity 
+            onPress={() => {}}>
+            <View style={styles.profileTile}>
+              <View style={styles.tileIcon}>
+                <MaterialCommunityIconsIcon 
+                  name="account-circle" style={styles.tileIcon}>
+                </MaterialCommunityIconsIcon>
+              </View>
+              <View>
+                <Text style={styles.tileText}>PROFILE</Text>
+              </View>
             </View>
           </TouchableOpacity>
-          : null}
-        <TouchableOpacity style={styles.historybtn}
-          onPress={() => props.navigation.navigate('History')}
-        >
-          <View style={styles.historyicnStack}>
-            <MaterialCommunityIconsIcon
-              name="history"
-              style={styles.historyicn}
-            ></MaterialCommunityIconsIcon>
-            <Text style={styles.history}>History</Text>
-          </View>
-        </TouchableOpacity>
+        </View>
       </ImageBackground>
-    </View>
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#002657'
   },
-  image: {
-    flex: 1
+  /* FIRST BLOCK */
+  firstBlock: {
+    flex: 1,
+    minHeight: '20%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  image_imageStyle: {},
-  profilebtn: {
-    width: 290,
-    height: 116,
-    backgroundColor: "rgba(74,144,226,0.9)",
-    borderRadius: 17,
-    marginTop: 68,
-    alignSelf: "center"
+  menuIconBlock: {
+    width: 50,
+    height: 50,
+    padding: 5,
+    position: 'absolute',
+    left: 0,
+    top: 0
   },
-  profileicn: {
-    top: 0,
-    position: "absolute",
-    color: "rgba(255,255,255,1)",
-    fontSize: 60,
-    left: 19,
-    height: 66,
-    width: 60
+  menuIcon: {
+    width: 50,
+    height: 50,
+    fontSize: 40,
+    color: 'white'
   },
-  profile: {
-    top: 59,
-    position: "absolute",
-    fontFamily: "roboto-300",
-    color: "#121212",
-    fontSize: 30,
-    textAlign: "center",
-    left: 0
+  userBanner: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: '5%'
   },
-  profileicnStack: {
-    width: 97,
-    height: 101,
-    marginTop: 2,
-    marginLeft: 96
-  },
-  checkinbtn: {
-    width: 290,
-    height: 117,
-    backgroundColor: "rgba(126,211,33,0.9)",
-    borderRadius: 17,
-    marginTop: 19,
-    alignSelf: "center"
-  },
-  checkinicn: {
-    top: 0,
-    position: "absolute",
-    color: "rgba(255,255,255,1)",
-    fontSize: 60,
-    left: 35,
-    height: 66,
-    width: 60
-  },
-  checkIn: {
-    top: 61,
-    position: "absolute",
-    fontFamily: "roboto-300",
-    color: "#121212",
-    fontSize: 30,
-    textAlign: "center",
-    left: 0
-  },
-  checkinicnStack: {
-    width: 131,
-    height: 103,
-    marginTop: 5,
-    marginLeft: 80
-  },
-  checkoutbtn: {
-    width: 290,
-    height: 117,
-    backgroundColor: "rgba(248,231,28,0.9)",
-    borderRadius: 17,
-    marginTop: 20,
-    alignSelf: "center"
-  },
-  checkouticn: {
-    top: 0,
-    position: "absolute",
-    color: "rgba(255,255,255,1)",
-    fontSize: 60,
-    left: 47,
-    height: 66,
-    width: 60
-  },
-  checkOut: {
-    top: 58,
-    position: "absolute",
-    fontFamily: "roboto-300",
-    color: "#121212",
-    fontSize: 30,
-    textAlign: "right",
-    left: 0
-  },
-  checkouticnStack: {
-    width: 154,
+  profileIconBlock: {
+    width: 100,
     height: 100,
-    marginTop: 3,
-    marginLeft: 68
+    backgroundColor: 'white',
+    borderRadius: 10,
+    shadowColor: '#00000',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  historybtn: {
-    width: 290,
-    height: 117,
-    backgroundColor: "rgba(155,155,155,0.9)",
-    borderRadius: 17,
-    marginTop: 22,
-    alignSelf: "center"
+  userIcon: {
+    width: 100,
+    height: 100,
   },
-  historyicn: {
-    top: 0,
-    position: "absolute",
-    color: "rgba(255,255,255,1)",
+  greetingTextBlock: {
+    height: 150,
+    width: 250,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    lineHeight: 10
+  },
+  greetingText: {
+    fontSize: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    letterSpacing: 2,
+    color: 'white'
+  },
+  /* SECOND BLOCK */
+  secondBlock: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '6%',
+    minHeight: '80%',
+    borderTopLeftRadius: 40, 
+    borderTopRightRadius: 40,
+    shadowColor: '#BBBBBB',
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.50,
+    shadowRadius: 12.35,
+    elevation: 19
+  },
+  tileBlock: {
+    flex: 1,
+  },
+  tileIcon: {
     fontSize: 60,
-    left: 24,
-    height: 66,
-    width: 60
+    height: 60,
+    width: 60,
+    color: '#ffffff'
   },
-  history: {
-    top: 56,
-    position: "absolute",
-    fontFamily: "roboto-300",
-    color: "#121212",
-    fontSize: 30,
-    left: 0
+  tileText: {
+    color: '#ffffff',
+    paddingLeft: 20,
+    fontSize: 25,
+    letterSpacing: 2
   },
-  historyicnStack: {
-    width: 110,
-    height: 98,
-    marginTop: 6,
-    marginLeft: 91
+  checkInTile: {
+    backgroundColor: '#003060',
+    minWidth: '80%',
+    minHeight: '15vh',
+    marginTop: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10
+  },
+  checkOutTile: {
+    backgroundColor: '#055C9D',
+    minWidth: '80%',
+    minHeight: '15vh',
+    marginTop: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10
+  },
+  historyTile: {
+    backgroundColor: '#0E86D4',
+    minWidth: '80%',
+    minHeight: '15vh',
+    marginTop: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+    padding: 10
+  },
+  profileTile: {
+    backgroundColor: '#68BBE3',
+    minWidth: '80%',
+    minHeight: '15vh',
+    marginTop: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+    padding: 0
   }
+
 });
 
 export default Dashboard;
